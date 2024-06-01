2c90b4f4173ce7143df69eb0829d7c1a5778d972
Akhil Vasanth <akhilpvasanth@gmail.com>
2024-06-01 15:41:12 -0700

    currently trying to integrate some of the Oz-Speller data collection task with our task

--- ssvep_speller/2task.py
+++ ssvep_speller/2task.py
@@ -2,9 +2,7 @@
 # !!! MAKE SURE refresh_rate IS SET TO YOUR MONITOR'S REFRESH RATE !!!
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-import psychopy.visual
-import psychopy.event
-import psychopy.core
+from psychopy import visual, event, core
 import time
 import numpy as np
 import pylsl
@@ -13,42 +11,71 @@ import csv
 import os
 import pandas as pd
 import screeninfo
+from psychopy.hardware import keyboard
+from scipy import signal
 # Global variables
 #
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # !!! MAKE SURE refresh_rate IS SET TO YOUR MONITOR'S REFRESH RATE !!!
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-win = None # Global variable for window (Initialized in main)
+monitor_info = screeninfo.get_monitors()[0] #gets info about you monitor (height, width, etc.)
+width = monitor_info.width # Width of your monitor in pixels
+height = monitor_info.height  # Height of your monitor in pixels
+keyboard_classes = [(8, 0), (10, 0), (12, 0), (14, 0),]
+stim_duration = 5  # in seconds
+n_keyboard_classes = len(keyboard_classes)
+classes = keyboard_classes
+refresh_rate = 60.003  # refresh rate of the monitor
+use_retina = False  # whether the monitor is a retina display
+win = visual.Window(
+        size=[width, height],
+        checkTiming=True,
+        allowGUI=False,
+        fullscr=True,
+        useRetina=use_retina
+    ) # Global variable for window (Initialized in main)
 mrkstream_out = None # Global variable for LSL marker stream output (Initialized in main)
 results_in = None # Global variable for LSL result input (Initialized in main)
 fixation = None # Global variable for fixation cross (Initialized in main)
 training_mode = True # train the model?
 GUI_only_mode = True
 bg_color = [0, 0, 0]
-monitor_info = screeninfo.get_monitors()[0] #gets info about you monitor (height, width, etc.)
-width = monitor_info.width # Width of your monitor in pixels
-height = monitor_info.height  # Height of your monitor in pixels
 
 cursor_mode = True
 #========================================================
 # High Level Functions
 #========================================================
-def Paradigm():
-    win_w = 2736/2
-    win_h = 1824/2
-    win_size = [win_w, win_h]  # Window size    
-    win = psychopy.visual.Window(size = (width, height), units="pix", color=[-1, -1, -1], fullscr=True)
-        #refresh_rate = 60
+def create_4_keys(size=120, colors=[-1, -1, -1] * 4):
+    #positions = []
+    #positions.extend([[-width / 2 + 190 * 3 + 150, height / 2 - 90 - i * 200 - 80] for i in range(4)])
+    key_positions = [(0, height/2 - 50), (width/2 - 50, 0), (0, -height/2 + 50), (-width/2 + 50, 0)]
+    keys = visual.ElementArrayStim(win, nElements=4, elementTex=None, elementMask=None, units='pix',
+                                   sizes=[size, size], xys=key_positions, colors=colors)
+    return keys
 
-    refresh_rate = win.getActualFrameRate()
-    if refresh_rate is None:
-        print("Could not measure frame rate, assuming 60Hz")
-        refresh_rate = 60
-    # refresh_rate = 165. # Monitor refresh rate (CRITICAL FOR TIMING)
-    # Define the squares (stimuli)
-    stim_freqs = [8, 9, 10, 11]  # Frequencies for SSVEP stimuli in Hz
-    phase_shifts = [0.5, 0, 0, 0]
+def Paradigm():
+    # win_w = 2736/2
+    # win_h = 1824/2
+    # win_size = [win_w, win_h]  # Window size    
+    # win = psychopy.visual.Window(size = (width, height), units="pix", color=[-1, -1, -1], fullscr=True)
+    #     #refresh_rate = 60
+
+    # refresh_rate = win.getActualFrameRate()
+    # if refresh_rate is None:
+    #     print("Could not measure frame rate, assuming 60Hz")
+    #     refresh_rate = 60
+    # # refresh_rate = 165. # Monitor refresh rate (CRITICAL FOR TIMING)
+    # # Define the squares (stimuli)
+    stim_freqs = [8, 10, 12, 14]  # Frequencies for SSVEP stimuli in Hz
+    phase_shifts = [0, 0, 0, 0]
+
+    keyboard_classes = [(8, 0), (10, 0), (12, 0), (14, 0),]
+    stim_duration = 5  # in seconds
+    n_keyboard_classes = len(keyboard_classes)
+    classes = keyboard_classes
+    refresh_rate = 60.003  # refresh rate of the monitor
+    use_retina = False  # whether the monitor is a retina display
 
     # makes random set of training squares and exports to csv file
     training_length = 20
@@ -69,90 +96,126 @@ def Paradigm():
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    ######################## making gui
+    # ######################## making gui
 
-    stimuli = []
+    # stimuli = []
 
-    if cursor_mode == True:
-        key_positions = [(0, height/2 - 50), (width/2 - 50, 0), (0, -height/2 + 50), (-width/2 + 50, 0)]
-    else:
-        key_positions = [(-width/4, height/4), (width/4, height/4), (-width/4, -height/4), (width/4, -height/4)]  # Positions for each key/square
+    # if cursor_mode == True:
+    #     key_positions = [(0, height/2 - 50), (width/2 - 50, 0), (0, -height/2 + 50), (-width/2 + 50, 0)]
+    # else:
+    #     key_positions = [(-width/4, height/4), (width/4, height/4), (-width/4, -height/4), (width/4, -height/4)]  # Positions for each key/square
     
-    # Create square stimuli
-    stimuli = []
+    # # Create square stimuli
+    # stimuli = []
+    training_stimuli = []
+    # for i, pos in enumerate(key_positions):
+    # #     square = psychopy.visual.Rect(win, width=100, height=100, pos=pos, fillColor=[1, 1, 1], lineColor=None)
+    #     training_square = psychopy.visual.Rect(win, width=100, height=100, pos=pos, fillColor=None, lineColor="red", lineWidth=10)
+    # #     stimuli.append(square)
+    #     training_stimuli.append(training_square)
+
+    # # Main experiment loop
+    # terminate1 = False # first while loop if key pressed
+    # terminate2 = False # second while loop if key pressed
+    # k = 0
+    # while not terminate1 and k < training_length:
+    #     while not terminate2 and k < training_length:
+    #         # INTERSTIMULATORY INVERVAL (NO FLASHING)
+    #         # 1000ms no flashing
+    #         for frame in range(MsToFrames(1000, refresh_rate)):
+    #             for stim_index, square in enumerate(stimuli):
+    #                 if training_mode:
+    #                     square.draw()
+    #                     training_stimuli[red_square_seq[k]].draw()
+    #                     #update meta dataframe with time red square was shown 
+    #                     training_sequence.at[k, "time"] = pylsl.local_clock()
+    #                 else:
+    #                     square.draw()
+                    
+
+    #             # Break if there's a keyboard event
+    #             if len(psychopy.event.getKeys()) > 0:
+    #                 terminate1 = True
+    #                 terminate2 = True
+    #             if GUI_only_mode != True:
+    #                 mrkstream_out.push_sample(pylsl.vectorstr(["0"])) # isi marker
+
+    #             win.flip()
+    #         # STIMULATORY INTERVAL (FLASHING)
+    #         # 1500ms flicker square stim
+    #         for frame in range(MsToFrames(1500, refresh_rate)):
+    #             current_time = psychopy.core.getTime()
+    #             for stim_index, square in enumerate(stimuli):
+    #                 if GUI_only_mode != True:
+    #                     mrkstream_out.push_sample(pylsl.vectorstr(["16"])) # si marker
+
+    #                 if int(current_time * stim_freqs[stim_index]) % 2 == 0: #draw flickers 
+    #                     square.draw()
+                    
+
+    #             # Break if there's a keyboard event
+    #             if len(psychopy.event.getKeys()) > 0:
+    #                 terminate1 = True
+    #                 terminate2 = True
+    #                 break
+
+    #             win.flip()
+    #         k+=1
+
+    #     """ # Wait until we get a valid result from the backend
+    #     results = None
+    #     print('Looking for result')
+    #     while results is None:
+    #         results, t = results_in.pull_sample(timeout=0)  
+    #         if results != None:
+    #             break
+        
+    #     # Once results found, display them
+    #     text.text = f'Classifier returned: {results[0]}'
+    #     print(f'{text.text}')
+    #     for frame in range(MsToFrames(1000, refresh_rate)):
+    #         text.draw()
+    #         win.flip() """
+
+    kb = keyboard.Keyboard()
+
+    key_positions = [(0, height/2 - 50), (width/2 - 50, 0), (0, -height/2 + 50), (-width/2 + 50, 0)]
+    flickering_keyboard = create_4_keys()
     training_stimuli = []
     for i, pos in enumerate(key_positions):
-        square = psychopy.visual.Rect(win, width=100, height=100, pos=pos, fillColor=[1, 1, 1], lineColor=None)
-        training_square = psychopy.visual.Rect(win, width=100, height=100, pos=pos, fillColor=None, lineColor="red", lineWidth=10)
-        stimuli.append(square)
+    #     square = psychopy.visual.Rect(win, width=100, height=100, pos=pos, fillColor=[1, 1, 1], lineColor=None)
+        training_square = visual.Rect(win, width=130, height=130, pos=[0,0], fillColor=None, lineColor="red", lineWidth=30)
+    #     stimuli.append(square)
         training_stimuli.append(training_square)
 
-    # Main experiment loop
-    terminate1 = False # first while loop if key pressed
-    terminate2 = False # second while loop if key pressed
-    k = 0
-    while not terminate1 and k < training_length:
-        while not terminate2 and k < training_length:
-            # INTERSTIMULATORY INVERVAL (NO FLASHING)
-            # 1000ms no flashing
-            for frame in range(MsToFrames(1000, refresh_rate)):
-                for stim_index, square in enumerate(stimuli):
-                    if training_mode:
-                        square.draw()
-                        training_stimuli[red_square_seq[k]].draw()
-                        #update meta dataframe with time red square was shown 
-                        training_sequence.at[k, "time"] = pylsl.local_clock()
-                    else:
-                        square.draw()
-                    
+    stim_duration_frames = MsToFrames((stim_duration) * 1000,
+                                       refresh_rate)  # total number of frames for the stimulation
+    frame_indices = np.arange(stim_duration_frames)  # the frames as integer indices
+    flickering_frames = np.zeros((len(frame_indices), n_keyboard_classes))
+    for i_class, (flickering_freq, phase_offset) in enumerate(keyboard_classes):
+        phase_offset += .00001  # nudge phase slightly from points of sudden jumps for offsets that are pi multiples
+        flickering_frames[:, i_class] = signal.square(2 * np.pi * flickering_freq * (
+                    frame_indices / refresh_rate) + phase_offset * np.pi)  # frequency approximation formula
+    
+    while True:
+        keys = kb.getKeys()
+        for thisKey in keys:
+            if thisKey == 'escape':
+                core.quit()
+        for i_frame, frame in enumerate(flickering_frames):
+            next_flip = win.getFutureFlipTime()
+            flickering_keyboard.colors = np.array([frame] * 3).T
+            training_square.draw()
+            flickering_keyboard.draw()
+            #training_stimuli[0].draw()
+            
+            win.flip()
 
-                # Break if there's a keyboard event
-                if len(psychopy.event.getKeys()) > 0:
-                    terminate1 = True
-                    terminate2 = True
-                if GUI_only_mode != True:
-                    mrkstream_out.push_sample(pylsl.vectorstr(["0"])) # isi marker
 
-                win.flip()
-            # STIMULATORY INTERVAL (FLASHING)
-            # 1500ms flicker square stim
-            for frame in range(MsToFrames(1500, refresh_rate)):
-                current_time = psychopy.core.getTime()
-                for stim_index, square in enumerate(stimuli):
-                    if GUI_only_mode != True:
-                        mrkstream_out.push_sample(pylsl.vectorstr(["16"])) # si marker
-
-                    if int(current_time * stim_freqs[stim_index]) % 2 == 0: #draw flickers 
-                        square.draw()
-                    
-
-                # Break if there's a keyboard event
-                if len(psychopy.event.getKeys()) > 0:
-                    terminate1 = True
-                    terminate2 = True
-                    break
-
-                win.flip()
-            k+=1
-
-        """ # Wait until we get a valid result from the backend
-        results = None
-        print('Looking for result')
-        while results is None:
-            results, t = results_in.pull_sample(timeout=0)  
-            if results != None:
-                break
-        
-        # Once results found, display them
-        text.text = f'Classifier returned: {results[0]}'
-        print(f'{text.text}')
-        for frame in range(MsToFrames(1000, refresh_rate)):
-            text.draw()
-            win.flip() """
 
     ############################## Export to CSV
 
-    psychopy.event.clearEvents()
+    event.clearEvents()
     win.close()
     # Get the list of existing files in the directory
     existing_files = os.listdir(directory)
@@ -202,20 +265,22 @@ if __name__ == "__main__":
     # Set random seed
     random.seed()
 
-    if GUI_only_mode != True:
-        # Initialize LSL marker streams
-        mrkstream_out = lsl_mrk_outlet('Task_Markers') # important this is first
-        results_in = lsl_inlet('Result_Stream')
+    # if GUI_only_mode != True:
+    #     # Initialize LSL marker streams
+    #     mrkstream_out = lsl_mrk_outlet('Task_Markers') # important this is first
+    #     results_in = lsl_inlet('Result_Stream')
 
-        # Wait a second for the streams to settle down
-        time.sleep(1)
+    #     # Wait a second for the streams to settle down
+    #     time.sleep(1)
 
-        # wait for markerstream to be used by LabRecorder
-        while not mrkstream_out.have_consumers():
-            psychopy.core.wait(0.2)
+    #     # wait for markerstream to be used by LabRecorder
+    #     while not mrkstream_out.have_consumers():
+    #         core.wait(0.2)
 
-        # Run through paradigm
-        if mrkstream_out.have_consumers():
-            Paradigm()
-    else:
-        Paradigm()
+    #     # Run through paradigm
+    #     if mrkstream_out.have_consumers():
+    #         Paradigm()
+    # else:
+    #     Paradigm()
+
+    Paradigm()
